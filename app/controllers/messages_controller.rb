a96e49f55f3c1234672bfc17f4348989308386f7
Samy Chalakani <samychalakani@gmail.com>
2018-06-12 16:04:14 +0200

    :id

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 class MessagesController < ApplicationController
-:id  before_action :authenticate_user!
+before_action :authenticate_user!
 
 def new
   @chosen_recipient = User.find_by(id: params[:to].to_i) if params[:to]
